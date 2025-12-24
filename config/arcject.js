d6e15641cc363fb12d1a40b1896c01c48b63edbe
Elisa Alvarez <elisaalvarez211@gmail.com>
2025-12-23 18:30:09 -0800

    added arcjet rate limiting

---
+++ config/arcject.js
@@ -0,0 +1,23 @@
+import arcjet, {shield, detectBot, tokenBucket} from "@arcjet/node";
+import { ARCJET_KEY } from "../config/env.js";
+const aj = arcjet({
+  key: ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE", 
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        "CATEGORY:MONITOR", // Uptime monitoring services
+        "CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+      ],
+    }),
+    // Create a token bucket rate limit. Other algorithms are supported.
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5, // Refill 5 tokens per interval
+      interval: 10, // Refill every 10 seconds
+      capacity: 10, // Bucket capacity of 10 tokens
+    }),
+  ],
+});
